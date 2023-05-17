40a7da81fdc419a0a2b2d5804a72aaeea7de09a0
MicheloMweemba <michelomweemba77@gmail.com>
2023-05-17 12:29:29 +0200

    A funtion that adds two numbers

---
+++ Add.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  function Addition ( $numb1,$numb2){
+  $sum = $numb1 + $numb2;
+  return $sum;
+  }
+$Result = Addition(9,9);
+echo $Result
+  ?>  
+</body>
+</html>
